66ebfaae0ea8690abdffd9837ce45779eaa48481
qkrwlstjd <qkrwls91877276@gmail.com>
2024-01-17 01:59:22 +0900

    Refactor 'scheduler' app to 'stock' with new models
    The 'scheduler' app was renamed to 'stock' and related files were moved accordingly. New django_cron library is installed and 'scheduler.apps.SchedulerConfig' is replaced with 'stock.apps.SchedulerConfig'. A new file was created called 'finalizedPriceCronJob.py' with a pass declared class PriceFinalizationCronJob. Additionally, new models 'Ticker' and 'Price' were created in 'stock' app.

--- quantProject/settings.py
+++ quantProject/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scheduler.apps.SchedulerConfig',
+    #installed
+    'django_cron',
+    #apps
+    'stock.apps.SchedulerConfig',
     'analytics.apps.AnalyticsConfig',
 ]
 
---
+++ stock/cron/finalizedPriceCronJob.py
@@ -0,0 +1,6 @@
+from django_cron import CronJobBase
+
+
+class PriceFinalizationCronJob(CronJobBase):
+    """This job handles the finalization of prices"""
+    pass
